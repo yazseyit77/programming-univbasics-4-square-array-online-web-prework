c9733f6ab359ab65fe859574a9a70ad8724cd091
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-01 18:01:12 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+    counter = 0
+    result = []
+    while counter < array.length do
+      result[counter] = array[counter] ** 2
+      counter += 1
+    end
+    return result
 end
